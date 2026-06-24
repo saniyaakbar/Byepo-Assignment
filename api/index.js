a3e325c27cb3ba630b116f6df64e0ee55e97bf53
saniyaakbar <saniyaakbar951@gmail.com>
2026-06-24 15:58:08 +0530

    vercel deployment backend

--- api/index.js
+++ api/index.js
@@ -1,8 +1,5 @@
-/**
- * Vercel Serverless Function Handler
- * Exports Express app as default handler for Vercel
- */
-
 import app from '../backend/src/index.js';
 
-export default app;
+export default function handler(req, res) {
+  return app(req, res);
+}
